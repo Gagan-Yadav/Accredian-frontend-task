9530006bec2144e6ed788d329b788276ecbaa871
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2024-07-20 16:16:27 +0530

    Initial Commit

---
+++ refer-earn-frontend/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import ReferEarnPage from './ReferEarnPage';
+import Footer from './Footer';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <ReferEarnPage />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ refer-earn-frontend/src/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Typography, Link } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <Box component="footer" py={3} mt={5} bgcolor="lightgray" textAlign="center">
+      <Typography variant="body1">
+        <Link href="https://your-portfolio-link.com" target="_blank" rel="noopener noreferrer">
+          My Portfolio
+        </Link>
+      </Typography>
+      <Typography variant="body1">
+        <Link href="https://github.com/gagan-yadav" target="_blank" rel="noopener noreferrer">
+           My GitHub
+        </Link>
+      </Typography>
+      <Typography variant="body1">
+        Work Samples:
+      </Typography>
+      <Typography variant="body1">
+        <Link href="https://linkedin-clone-by-gagan.vercel.app/" target="_blank" rel="noopener noreferrer">
+          1. LinkedIn Clone
+        </Link>
+        
+      </Typography>
+      <Typography variant="body1">
+      <Link href="https://vvuniqueinfotech.vercel.app/" target="_blank" rel="noopener noreferrer">
+           2. VvUniqueInfotech
+        </Link>
+      </Typography>
+      
+      <Typography variant="body2" color="textSecondary" mt={2}>
+        &copy; {new Date().getFullYear()} Gagan Yadav. All rights reserved.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
